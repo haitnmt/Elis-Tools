cbe09d0beef029835112769a7daa81d236f30d00
haitnmt <haitnmt@gmail.com>
2024-12-08 14:08:37 +0700

    Create MAUI

---
+++ src/Haihv.Elis.Tools.ChuyenDvhc/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Haihv.Elis.Tools.ChuyenDvhc;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
