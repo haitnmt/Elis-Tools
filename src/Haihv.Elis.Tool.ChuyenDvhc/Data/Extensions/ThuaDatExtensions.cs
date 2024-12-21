bd6247c2ff73990b5b0ffe801bc13f1680f5f856
haitnmt <haitnmt@gmail.com>
2024-12-21 15:55:19 +0700

    Add TimeLineProcess component and ThuaDatExtensions for database operations

---
+++ src/Haihv.Elis.Tool.ChuyenDvhc/Data/Extensions/ThuaDatExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Haihv.Elis.Tool.ChuyenDvhc.Data.Extensions;
+
+public static class ThuaDatExtensions
+{
+    public static async Task<int> GetCountAsync(this ElisDataContext dbContext, List<long> toBanDos)
+    {
+        return await dbContext.ThuaDats.CountAsync(x => toBanDos.Contains(x.MaToBanDo));
+    }
+
+}
